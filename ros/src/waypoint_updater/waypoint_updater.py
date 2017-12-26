394e0a878b13a82d8175713d11817e3f96c7bdd8
Matthew Younkins <matthewyounkins@protonmail.com>
2017-12-26 14:20:00 -0800

    Corrected tabs
    Cotton pickin' unix tabs are eight spaces wide.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -23,49 +23,49 @@ as well as to verify your TL classifier.
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
 
-
+rospy.logwarn("Test")
 class WaypointUpdater(object):
     def __init__(self):
         rospy.init_node('waypoint_updater')
-		
-	#Available rostopic list (unused subscriptions commented out)
-        rospy.Subscriber('/base_waypoints', 	Lane, 		self.base_waypoints_cb)
-	rospy.Subscriber('/current_pose',   	PoseStamped,	self.current_pose_cb)  
-	rospy.Subscriber('/current_velocity', 	TwistStamped,	self.current_velocity_cb)
-	#rospy.Subscriber('/image_color', 	Lane,		'''needs self.something''')
-	#rospy.Subscriber('/rosout'		Lane,		'''needs self.something''')
-	#rospy.Subscriber('/rosout_agg, 	Lane,		'''needs self.something''')
-	#rospy.Subscriber('/tf			tfMessage,	'''needs self.something''')
-	rospy.Subscriber('/traffic_waypoint', 	Int32, 		self.traffic_waypoint_cb)
-	#rospy.Subscriber('/twist_cmd, 		TwistStamped,	'''needs self.something''')
-			
-	#I'm not sure yet how to get the vehicle subscriber working ...
-	#rospy.Subscriber('/vehicle/brake_cmd,		dbw_mkz_msgs/BrakeCmd,		'''needs self.something''')
-	#rospy.Subscriber('/vehicle/steering_cmd,	dbw_mkz_msgs/SteeringCmd,	'''needs self.something''')
-	#rospy.Subscriber('/vehicle/throttle_cmd,	dbw_mkz_msgs/ThrottleCmd,	'''needs self.something''')
-	#rospy.Subscriber('/vehicle/traffic_lights,	TrafficLightArray,'''needs self.something''')
-	
-	#Matthew Younkins: I've commented out the line below because
-	#I don't know where this is coming from yet
-	#rospy.Subscriber('/obstacle_waypoint', 	Int32,	self.obstacle_waypoint_cb)  #ok
-		
-	self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)  
-
-	# Other member variables 
-
-	rospy.spin()
+        rospy.logwarn("Initializing Script")
+        #Available rostopic list (unused subscriptions commented out)
+        rospy.Subscriber('/base_waypoints',     Lane,           self.base_waypoints_cb)
+        rospy.Subscriber('/current_pose',       PoseStamped,    self.current_pose_cb)  
+        rospy.Subscriber('/current_velocity',   TwistStamped,   self.current_velocity_cb)
+        #rospy.Subscriber('/image_color',   Lane,       '''needs self.something''')
+        #rospy.Subscriber('/rosout'     Lane,       '''needs self.something''')
+        #rospy.Subscriber('/rosout_agg,     Lane,       '''needs self.something''')
+        #rospy.Subscriber('/tf          tfMessage,  '''needs self.something''')
+        rospy.Subscriber('/traffic_waypoint',   Int32,      self.traffic_waypoint_cb)
+        #rospy.Subscriber('/twist_cmd,      TwistStamped,   '''needs self.something''')
+            
+        #I'm not sure yet how to get the vehicle subscriber working yet ...
+        #rospy.Subscriber('/vehicle/brake_cmd,      dbw_mkz_msgs/BrakeCmd,      '''needs self.something''')
+        #rospy.Subscriber('/vehicle/steering_cmd,   dbw_mkz_msgs/SteeringCmd,   '''needs self.something''')
+        #rospy.Subscriber('/vehicle/throttle_cmd,   dbw_mkz_msgs/ThrottleCmd,   '''needs self.something''')
+        #rospy.Subscriber('/vehicle/traffic_lights, TrafficLightArray,'''needs self.something''')
+        
+        #Matthew Younkins: I've commented out the line until I can determine when to use it
+        #rospy.Subscriber('/obstacle_waypoint',     Int32,  self.obstacle_waypoint_cb)  #ok
+        
+        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)  
+        # Other member variables 
+        rospy.spin()
 
     def current_pose_cb(self, msg):
         self.current_pose = msg.pose
-        
+        #rospy.logwarn("Current Pose Called")
     def current_velocity_cb(self, msg):
         self.current_velocity = msg.twist.linear.x
-
+        #rospy.logwarn("Current Velocity Called")
     def base_waypoints_cb(self, Lane):
         self.base_waypoints = Lane.waypoints
         
-    def traffic_waypoint_cb(self, msg):   #I believe this is actually the red lights
+    def traffic_waypoint_cb(self, Int32):
+        # These are the red lights 
         self.traffic_waypoint = msg.data
+        rospy.logwarn("Traffic waypoint called")
+        
 
     def obstacle_cb(self, msg):
         self.obstacle_waypoint = None
