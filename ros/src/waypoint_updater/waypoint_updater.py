5208ed29a960433df59abadb72e69993d800237f
Matthew Younkins <matthewyounkins@protonmail.com>
2018-01-25 17:59:40 -0800

    Updater with brake that publishes closest point to ros

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -18,14 +18,14 @@ It will set a target velocity for those waypoints, using the traffic light waypo
 
 '''
 
-LOOKAHEAD_WPS = 100 # Number of waypoints we will publish. You can change this number
-VERBOSE = False
-DEVELOPER = False
-ARBITRARY_WAYPOINT_NUMBERING = False
-USE_TRAFFIC_LIGHTS = True
-BRAKING_DISTANCE_START = 30   #Intended to be the distance before the light at which braking starts 
-BRAKING_DISTANCE_END = 3      #Intended to be the distance before the light that the braking ends
-VELOCITY_TARGET = 11
+LOOKAHEAD_WPS                = 100    # Number of waypoints we will publish. You can change this number
+VERBOSE                      = False  # Use rospy.logwarn in a couple of places
+DEVELOPER                    = False  # Outputs time used by a subset of the algorithm
+ARBITRARY_WAYPOINT_NUMBERING = False  # Allows for non-sequential numbering system
+USE_TRAFFIC_LIGHTS           = True   # Uses Traffic light data
+BRAKING_DISTANCE_START       = 30     # Intended to be the distance before the light at which braking starts 
+BRAKING_DISTANCE_END         = 3      # Intended to be the distance before the light that the braking ends
+VELOCITY_TARGET              = 11     # Velocity Target!
 
 rospy.logwarn("Test")
 class WaypointUpdater(object):
@@ -41,6 +41,7 @@ class WaypointUpdater(object):
         #rospy.Subscriber('/rosout_agg,     Lane,       '''needs self.something''')
         #rospy.Subscriber('/tf          tfMessage,  '''needs self.something''')
         rospy.Subscriber('/traffic_waypoint',   Int32,          self.traffic_waypoint_cb)
+
         #rospy.Subscriber('/twist_cmd,      TwistStamped,   '''needs self.something''')
             
         #Likely not going to use the brake, steering, throttle command directly hereI
@@ -56,7 +57,10 @@ class WaypointUpdater(object):
         #Matthew Younkins: I've commented out the line until I can determine when to use it
         #rospy.Subscriber('/obstacle_waypoint',     Lane,  self.obstacle_waypoint_cb)  #ok
         self.base_waypoints_cb
-        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)  
+        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
+        self.closest_waypoint_pub = rospy.Publisher('closest_waypoint', Int32, queue_size=1)  
+        self.next_waypoint_pub = rospy.Publisher('next_waypoint', Int32, queue_size=1)  
+        
         self.old_point = 0
         self.older_point = -1
         # Other member variables 
@@ -86,6 +90,7 @@ class WaypointUpdater(object):
     def traffic_waypoint_cb(self, msg):
         # These are the red lights 
         self.traffic_waypoint = msg.data
+        rospy.logwarn(msg.data)
         if VERBOSE:
             rospy.logwarn("Traffic waypoint called, light message:" + str(self.traffic_waypoint))
         if self.traffic_waypoint >= 0:
@@ -103,28 +108,33 @@ class WaypointUpdater(object):
     def find_and_publish_final_waypoints(self):
         waypoints = self.base_waypoints
         total_points = enumerate(waypoints)
+        traffic_light_index = None
         #traffic_light_index = self.traffic_waypoint_cb
-        traffic_light_index = 350  #just an example here, need to remove this to get it to work
+
+        #traffic_light_index = 350  #just an example here, need to remove this to get it to work
         if DEVELOPER:
             t1 = time.time()
        
-        closest_waypoint_distance = float("inf")
+        closest_waypoint_distance_sq = float("inf")
         closest_point = -1
         
-        if traffic_light_index:
+        if traffic_light_index is not None:
             traffic_distance = math.sqrt((self.current_pose.position.x-waypoints[traffic_light_index].pose.pose.position.x)**2+
                                       (self.current_pose.position.y-waypoints[traffic_light_index].pose.pose.position.y)**2)
             #rospy.logwarn(traffic_distance)
         
-        for index, waypoint in enumerate(self.base_waypoints):
-            waypoint_distance = math.sqrt((self.current_pose.position.x-waypoint.pose.pose.position.x)**2+
-                                      (self.current_pose.position.y-waypoint.pose.pose.position.y)**2)
         
-            if (waypoint_distance < closest_waypoint_distance):
-                closest_waypoint_distance = waypoint_distance
+        for index, waypoint in enumerate(self.base_waypoints):
+            # no need to take the square root of here!  That reduced computation time from 18.6 to 18.4ms !!!!!!!!
+            # I know you're impressed.
+            waypoint_distance_sq = ((self.current_pose.position.x-waypoint.pose.pose.position.x)**2+
+                                      (self.current_pose.position.y-waypoint.pose.pose.position.y)**2)
+            #  waypoints[index].twist.twist.linear.z = index   #useful but not obvious, closest waypoint published on ros instead
+            if (waypoint_distance_sq < closest_waypoint_distance_sq):
+                closest_waypoint_distance_sq = waypoint_distance_sq
                 closest_point = index
                 
-            if traffic_distance < BRAKING_DISTANCE_START:
+            if (traffic_light_index is not None) and (traffic_distance < BRAKING_DISTANCE_START):
                 a = 1
                 vel = self.current_velocity
                 #rospy.logwarn('braking')
@@ -140,7 +150,7 @@ class WaypointUpdater(object):
             else:
                 waypoints[index].twist.twist.linear.x = VELOCITY_TARGET
                   
-
+        
         if DEVELOPER:
             t2 = time.time()
             rospy.logwarn("Time difference = " + str(t2-t1))
@@ -153,6 +163,7 @@ class WaypointUpdater(object):
         lane.header.stamp = rospy.Time(0)
         lane.waypoints =  self.base_waypoints[closest_point:closest_point+LOOKAHEAD_WPS]
         self.final_waypoints_pub.publish(lane)
+        self.closest_waypoint_pub.publish(closest_point)
         
 
 
